74ab0ff1de8a3a06fb1b74fce9174abfbb75df5f
Kristian <kristian.zh.apostolov@gmail.com>
2022-02-28 13:40:58 +0200

    Add routing to 'new' button in modules list.

--- src/app/modules/module.resolver.ts
+++ src/app/modules/module.resolver.ts
@@ -1,16 +1,17 @@
 import { Injectable } from "@angular/core";
 import { Router, Resolve, ActivatedRouteSnapshot } from "@angular/router";
-import {EMPTY, map, Observable} from "rxjs";
+import { EMPTY, map, Observable } from "rxjs";
 import { catchError } from "rxjs/operators";
-import {ModulesService} from "./modules.service";
-import {ModuleDetails} from "./module-detail/module-details.model";
+import { ModulesService } from "./modules.service";
+import { ModuleDetails } from "./module-detail/module-details.model";
 
 @Injectable({
     providedIn: "root",
 })
 export class ModuleResolver implements Resolve<ModuleDetails> {
 
-    constructor(private moduleService: ModulesService, private router: Router) {}
+    constructor(private moduleService: ModulesService, private router: Router) {
+    }
 
     resolve(route: ActivatedRouteSnapshot): Observable<ModuleDetails> {
         return this.moduleService.getModule(route.params?.id).pipe(
