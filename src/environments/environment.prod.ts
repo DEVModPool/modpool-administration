6ba55d01525797c2910d2aceb0b38dab5307b56a
Tin Dizdarevic <tinovmail@gmail.com>
2022-04-24 01:08:49 +0200

    Add production environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,24 @@
 export const environment = {
-  production: true
+    'jwt-key': 'jwt-admin',
+    'user-key': 'user-admin',
+    'userId-key': 'userId-admin',
+
+    production: true,
+    jwtAllowedDomain: 'administration.modpool.co.uk',
+    baseUrl: 'https://administration.modpool.co.uk/api/',
+
+    loginUrl: 'account/login',
+
+    departmentsUrl: 'departments/',
+    departmentsNewUrl: 'departments/new/',
+
+    modulesUrl: 'modules/',
+    modulesNewUrl: 'modules/new/',
+
+    usersUrl: 'users/',
+    usersNewUrl: 'users/new/',
+
+    reviewsUrl: 'reviews/',
+
+    resolverUrl: 'resolve-route/'
 };
